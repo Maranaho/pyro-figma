1bfa228da94db7ef16d821911972487dea7b6321
unknown <you@example.com>
2021-12-21 13:30:04 +0000

    auth design ok pt2

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -8,7 +8,7 @@ const Login = ()=>{
       <section>
         <h1>Welcome 😊</h1>
         <h2>Please sign in <br/>so we can let you in!</h2>
-        <p>Let me check if you  have access</p>
+        <p>Let me check if you  have access. <br/>You only have to do this once.</p>
         <SignIn/>
       </section>
     </main>
